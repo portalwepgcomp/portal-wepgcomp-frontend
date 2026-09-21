fd0a2268aa86df3991968ce1bbf641d5a1397e8a
Monique Silva <moniquesntsilva@gmail.com>
2026-09-21 20:20:07 -0300

    refactor(footer): altera cor de background

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-auto flex items-center justify-center bg-[#AAAAAA] py-2 text-center text-white">
+    <footer className="mt-auto flex items-center justify-center bg-brand-navy py-2 text-center text-white">
       <div className="text-base">© WEPGCOMP</div>
     </footer>
   );
